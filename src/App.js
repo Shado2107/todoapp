e7ca6e785ba936cb19481b719c3477ac9e6086f1
Shado2107 <pd39almeida@outlook.com>
2021-10-11 22:18:18 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import './style.css';
+import React, {useState} from 'react';
+import Form from './components/Form';
+import TodoList from './components/TodoList';
 
 function App() {
+
+  const [inputText, setInputText] = useState("");
+  const [Todos, setTodos] = useState([]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>Pavel todo list {inputText} </h1>
       </header>
+     <Form inputText={inputText} todos={Todos} setTodos={setTodos} setInputText={setInputText} />
+     <TodoList/>
     </div>
   );
 }
